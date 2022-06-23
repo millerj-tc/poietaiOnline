0513dd80782dc54bc162766e564e87860b1cc196
Joseph Miller <j.sam.miller@gmail.com>
2022-06-22 23:24:39 -0500

    poemCreatorDisplayBuild

---
+++ poemCreatorDisplayBuild/poemCreatorContentGatherer.js
@@ -0,0 +1,6 @@
+import {poemCreatorContentPasser} from "./poemCreatorContentPasser.js";
+
+export class poemCreatorContentGatherer
+{
+    
+}
---
+++ poemCreatorDisplayBuild/poemCreatorContentPasser.js
@@ -0,0 +1,45 @@
+import {poemCreatorDisplayBuilder} from "./poemCreatorDisplayBuilder.js";
+
+export class poemCreatorContentPasser
+{
+    static PassContentFlow(contentArr){ //called by poemCreatorContentGatherer
+        
+        const $parsedContentArr = this._ParseContent(contentArr);
+        
+        const $divArr = this._BuildContentDivs($parsedContentArr);
+        
+        this._PassContentDivsToPoemContentDisplay($divArr);
+    }
+    
+    static _ParseContent(contentArr){
+                
+        const $returnContentArr = contentArr;
+        
+        // if you need to do anything to a piece of content that will go here
+        
+        return $returnContentArr
+        
+    }
+    
+    static _BuildContentDivs(contentArr){
+        
+        let $returnArr = [];
+        
+        for(const content of contentArr){
+            
+            const $div = document.createElement("div");
+            
+            $div.innerHTML = "test";
+            
+            $returnArr.push $div
+        }
+        
+        return $returnArr
+        
+    }
+    
+    static _PassContentDivsToPoemContentDisplay(divArr){
+        
+        poemCreatorDisplayBuilder.LoadContentDivsFlow(divArr);
+    }
+}
