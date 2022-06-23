6f903d9697ca6d9adde9c958442c75f6e25c5652
Joseph Miller <j.sam.miller@gmail.com>
2022-06-23 08:12:28 -0500

    comment

--- poemCreator/poemCreatorDisplayUpdater.js
+++ poemCreator/poemCreatorDisplayUpdater.js
@@ -19,4 +19,6 @@ export class poemCreatorDisplayUpdater
         
         
     }
+    
+    // this should call Poem Output Display Updater -- use a callback in higher level controller flow if you have to wait for something to finish and before calling an already called class rather than spaghetti coding it
 }
