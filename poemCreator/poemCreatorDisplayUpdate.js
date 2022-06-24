d1cb7ad8a57fc2fab338f84838055f833bc78afb
Joseph Miller <j.sam.miller@gmail.com>
2022-06-23 23:08:20 -0500

    static class to function

--- poemCreator/poemCreatorDisplayUpdate.js
+++ poemCreator/poemCreatorDisplayUpdate.js
@@ -1,24 +1,21 @@
-export class poemCreatorDisplayUpdater
-{
-    static ContentDivPressed(div){
-        
-        console.log("You clicked " + div.innerHTML);
-    }
-    
-    static _GetPressedDivPos(){
-        
-        
-    }
-    
-    static _GetPlayerIndicatorDivPos(){
-        
-        
-    }
-    
-    static _CheckIfLineFromPlayerToPressedIsUnobstructed(){
-        
-        
-    }
-    
-    // this should call Poem Output Display Updater -- use a callback in higher level controller flow if you have to wait for something to finish and before calling an already called class rather than spaghetti coding it
+function ContentDivPressed(div){
+
+    console.log("You clicked " + div.innerHTML);
 }
+
+function _GetPressedDivPos(){
+
+
+}
+
+function _GetPlayerIndicatorDivPos(){
+
+
+}
+
+function _CheckIfLineFromPlayerToPressedIsUnobstructed(){
+
+
+}
+
+// this should call Poem Output Display Updater -- use a callback in higher level controller flow if you have to wait for something to finish and before calling an already called class rather than spaghetti coding it
