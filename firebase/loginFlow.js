a46853257be1a34e18284d91d3ca436ece60f08f
millerj-tc <j.sam.miller@gmail.com>
2022-06-28 22:06:22 -0400

    deprecated function removed from login flow

--- firebase/loginFlow.js
+++ firebase/loginFlow.js
@@ -1,7 +1,7 @@
-import {CreatorGridCreationFlow} from "./../poemCreator/creatorGrid/creatorGridCreationFlow.js";
+
 
 export function LoginFlow(){
     
     console.log("login flowing");
-    CreatorGridCreationFlow();
+
 }
